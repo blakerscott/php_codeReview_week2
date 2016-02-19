c044f18ba8845c8f35ade1c10d7510fac02d622f
Blake Scott <blakerscott@gmail.com>
2016-02-19 09:29:36 -0800

    add english spec test text file

---
+++ src/WordCounter.php
@@ -0,0 +1,12 @@
+<?php
+	 class WordCounter
+		{
+
+		 function countWordFunction(){
+			 if 
+
+			}
+
+		}
+	}
+ ?>
---
+++ tests/WordCountTest.php
@@ -0,0 +1,23 @@
+<?php
+
+	require_once 'src/Class.php';
+
+	class WordCounterTest extends PHPUnit_Framework_TestCase
+	{
+
+		function test_countOneWord()
+		{
+		//Arrange
+		$test_WordCounter = new WordCounter;
+		$wordToCount = 'beowulf';
+		$sentence = 'beowulf';
+
+		//Act
+		$result = $test_WordCounter->countWordFunction($wordToCount, $sentence);
+
+		//Assert
+		$this->assertEquals('1', $result);
+		}
+	}
+
+?>

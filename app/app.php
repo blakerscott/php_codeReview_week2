4f299685d9dff97d08d5c8472bdeb89135408af4
Blake Scott <blakerscott@gmail.com>
2016-02-19 15:39:39 -0800

    add views and ability to display them

--- app/app.php
+++ app/app.php
@@ -1,13 +1,22 @@
-<?php 
+<?php
 
-	require_once __DIR__.'/../vendor/autoload.php'; 
+	require_once __DIR__.'/../vendor/autoload.php';
+	require_once __DIR__."/../src/RepeatCounter.php";
 
-	$app = new Silex\Application(); 
+	$app = new Silex\Application();
 
-	$app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views')); 
+	$app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
-	$app->get('/', function(){return 'Hello, World!';}); 
+	$app->get("/", function() use ($app) {
+	return $app['twig']->render('home.html.twig');
+	});
 
-	return $app; 
+	$app->post('/result', function() use ($app) {
+	  $new_counter = new RepeatCounter;
+		$new_input = $new_counter->CountRepeats($_POST['word'],$_POST['sentence']);
+	  return $app['twig']->render('result.html.twig', array('result' => $new_input));
+	});
+
+	return $app;
 
 ?>
